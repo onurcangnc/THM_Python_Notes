2a0fc208a09b4bb2f271fc33ec6df0b2bb7f8057
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2023-09-26 06:37:36 +0300

    WIP

---
+++ Functions.py
@@ -0,0 +1,25 @@
+
+#def sayHello(name):
+    #print(" Hello " + name + " ! Nice to meet you. ")
+
+#name_input = input(" What is your name ?")
+#name = str(name_input)
+
+#sayHello(name)
+
+#--------------------#
+#Calculate total cost using functions in Python#
+
+#def totalCost(item):
+    #if item == "Pineapple":
+        #return 3.99
+    #elif item == "Banana":
+        #return 1.99
+    #else:
+        #return 0
+
+#spent = 10
+#item = input(" Pineapple or Banana ?")
+
+#spent = spent + totalCost(item)
+#print(" Your total cost is", spent)
---
+++ thm.py
@@ -0,0 +1,48 @@
+#initialize variables
+
+#a = 1
+#if a == 1 or a > 10:
+    #print("a is either 1 or above 10")
+
+#name = "bob"
+#hungry = True
+
+#if name == "bob" and hungry == True:
+    #print(" Bob is hungry ")
+
+#elif name == "bob" and not hungry:
+    #print(" Bob is not hungry ")
+
+#else: #if all other if conditions are not met
+    #print(" Not sure who this is or if they are hungry ")
+
+#age = None
+#age = input(" How old are you ? ")
+
+#age = int(age)
+
+#if age < 17:
+    #print(" You are not old enough to drive car :)")
+
+#else:
+    #print(" You can drive !")
+
+
+# Loops
+
+#i = 1
+
+#while i <= 10:
+    #print(i)
+    #i = i + 1
+
+#Lists
+
+#websites = ["facebook.com", "google.com", "amazon.com"]
+#for sites in websites:
+    #print(sites)
+
+#Flow control
+#i = 0
+#for i in range(5):
+    #print(i)
